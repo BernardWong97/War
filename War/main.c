3dc21e909ae8e1c8bb3af830b7a4287453c0c9ce
Bernard Wong <ben.wc88@gmail.com>
2018-01-03 20:30:45 +0000

    Create a War game using C language
    The game is based on the card game War - where each player flips over	a card, highest card wins.

---
+++ War/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// The main where the game runs
+void main() {
+	// Variables
+	int numOfPlayers;
+
+	// Prompt input how many players are playing
+	printf("Please enter how many players are playing ( 2 - 10 ): ");
+	scanf("%d", &numOfPlayers);
+
+	while (numOfPlayers < 2 || numOfPlayers > 10) {
+		printf("Only 2 to 10 players are allowed, please try again.\n\n");
+		printf("Please enter how many players are playing ( 2 - 10 ): ");
+		scanf("%d", &numOfPlayers);
+	} // while loop validate number of players
+
+	getch();
+} // main
